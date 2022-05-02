2c98d51d98526378e6db3aadd95f9743511014f0
Winsey Li <winsey@storypark.com>
2022-05-02 16:07:26 +1200

    Update avdmanager's path to "cmdline-tools/latest/bin"

--- lib/fastlane/plugin/android_emulator/actions/android_emulator_action.rb
+++ lib/fastlane/plugin/android_emulator/actions/android_emulator_action.rb
@@ -8,14 +8,16 @@ module Fastlane
         sdk_dir = params[:sdk_dir]
         port = params[:port]
         adb = "#{sdk_dir}/platform-tools/adb"
+        cmdline_tools_latest="#{sdk_dir}/cmdline-tools/latest/bin"
 
         UI.message("Stopping emulator")
         system("#{adb} emu kill > /dev/null 2>&1 &")
         sleep(2)
 
         UI.message("Creating new emulator")
+        UI.message("Calling #{cmdline_tools_latest}/avdmanager create avd")
         FastlaneCore::CommandExecutor.execute(
-          command: "#{sdk_dir}/tools/bin/avdmanager create avd -n '#{params[:name]}' -f -k '#{params[:package]}' -d '#{params[:device]}'",
+          command: "#{cmdline_tools_latest}/avdmanager create avd -n '#{params[:name]}' -f -k '#{params[:package]}' -d '#{params[:device]}'",
           print_all: true,
           print_command: false
         )
